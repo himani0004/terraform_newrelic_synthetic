f4bf3942f7afbba39fc3562689ed259f769ca8bb
himani0004 <himanisharma0004@gmail.com>
2023-11-07 22:58:40 +0530

    changed code

--- main.tf
+++ main.tf
@@ -1,24 +1,13 @@
-resource "newrelic_synthetics_monitor" "himani_monitorr" {
+resource "newrelic_synthetics_monitor" "himani_monitor" {
   
   status           = "ENABLED"
-  name            =  "simple_monitor"
+  name            =  "simple_monitor_first"
   period           = "EVERY_MINUTE"
   uri              = "https://www.thinkitlearntechnology.com/"
   type             = "SIMPLE"
   locations_public = ["AP_SOUTH_1"]
 
-#   custom_header {
-#     name  = "some_name"
-#     value = "some_value"
-#   }
 
-  treat_redirect_as_failure = true
-  validation_string         = "success"
-  bypass_head_request       = true
-  verify_ssl                = true
 
-  tag {
-    key    = "some_key"
-    values = ["some_value"]
-  }
+  
 }
